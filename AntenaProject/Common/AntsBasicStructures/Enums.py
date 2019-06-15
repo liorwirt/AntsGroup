c20425431d7f5b50ff0d8dc7e7d4502d1a62f0cb
dngreenst <dngreenst@gmail.com>
2019-06-15 18:10:52 +0300

    Defined CellWeights.

--- AntenaProject/Common/AntsBasicStructures/Enums.py
+++ AntenaProject/Common/AntsBasicStructures/Enums.py
@@ -1,6 +1,5 @@
 from enum import IntEnum
 
-
 class NodeStateEnum(IntEnum):
     UnExplored=0,
     Clear=1,
@@ -11,3 +10,8 @@ class NodeStateEnum(IntEnum):
 class AntType(IntEnum):
     Scout = 0,
     Transmission = 1,
+
+
+class CellWeights:
+    UnexploredCell = 1.0
+    ExploredCell = 2.0
