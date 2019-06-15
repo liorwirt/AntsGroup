11870ceb1fc72ebb41cda09ff8a2505651146ee3
dngreenst <dngreenst@gmail.com>
2019-06-15 14:49:16 +0300

    AlgoAnt impl start

--- AntenaProject/AlgoAnt/AlgoAnt.py
+++ AntenaProject/AlgoAnt/AlgoAnt.py
@@ -1,5 +1,6 @@
 from typing import Tuple, Dict
 import numpy as np
+from AntenaProject.Common.AntsBasicStructures.Enums import AntType
 
 from AntenaProject.Common.AntsBasicStructures.BasicAnt import BasicAnt, Position, BaseSingleAntWorldImage, AntStep
 
@@ -9,7 +10,12 @@ class AlgoAnt(BasicAnt):
     def __init__(self, id: int, config, position: Position):
         super().__init__(id, config)
         super().UpdatePosition(position)
+        self.role = AntType.Scout
 
     def _internalGetStep(self, antworldstate: BaseSingleAntWorldImage) -> Tuple[Position, Dict]:
-        pass
+        fellowAnts = antworldstate.Ants()
+        mazeImage = antworldstate.VisibleNodes
+
+
+
 
